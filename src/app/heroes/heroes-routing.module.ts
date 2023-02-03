2ebcc5fa257d3c3a60c3960d9e42a03503f7058b
illika <steve.milan7@gmail.com>
2023-02-02 19:04:53 -0500

    ActivatedRoute - params

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
         component: ListadoComponent
       },
       {
-        path: "agergar",
+        path: "agregar",
         component: AgregarComponent
       },
       {
